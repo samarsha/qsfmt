744b93babb929147dc7308b9dfab676451b76d84
Sarah Marshall <sarah@sarahmarshall.name>
2020-09-26 13:12:15 -0700

    Move example program to App project

--- App/Program.fs
+++ App/Program.fs
@@ -1,6 +1,14 @@
 ﻿open QsFmt.Formatter
 
+let private example = "\
+namespace Foo {
+    function Bar() : Int {
+        let x = 7;
+        return x;
+    }
+}"
+
 [<EntryPoint>]
 let main _ =
-    Formatter.format ()
+    Formatter.format example
     0
--- Formatter/Formatter.fs
+++ Formatter/Formatter.fs
@@ -4,19 +4,7 @@ open Antlr4.Runtime
 open QsFmt.Formatter.ParseTree
 open QsFmt.Parser
 
-let private example =
-    "namespace Foo {
-         function Bar() : Int {
-             let x = 7;
-             return x;
-         }
-     }"
-
-let format () =
-    let tokens =
-        example
-        |> AntlrInputStream
-        |> QSharpLexer
-        |> CommonTokenStream
+let format (source : string) =
+    let tokens = source |> AntlrInputStream |> QSharpLexer |> CommonTokenStream
     let parser = QSharpParser tokens
     parser.program () |> toProgramNode tokens |> printfn "%A"
