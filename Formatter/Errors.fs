ce8ca4cba0ff121ec0d42988c24b5fc5286e0980
Sarah Marshall <sarah@sarahmarshall.name>
2020-11-07 20:47:09 -0800

    Update code style in Errors.fs

--- Formatter/Errors.fs
+++ Formatter/Errors.fs
@@ -9,16 +9,14 @@ type ErrorListListener () =
     member _.ErrorTokens = errorTokens
 
     interface IToken IAntlrErrorListener with
-        override _.SyntaxError (_, _, token, _, _, _, _) =
-            errorTokens <- token :: errorTokens
+        override _.SyntaxError (_, _, token, _, _, _, _) = errorTokens <- token :: errorTokens
 
 let private hideToken (token : IToken) : IToken =
     let token' = CommonToken token
     token'.Channel <- QSharpLexer.Hidden
     upcast token'
 
-let hideTokens toHide =
-    Seq.map <| fun token ->
-        if toHide |> Seq.contains token
-        then hideToken token
-        else token
+let hideTokens toHide = Seq.map <| fun token ->
+    if toHide |> Seq.contains token
+    then hideToken token
+    else token
