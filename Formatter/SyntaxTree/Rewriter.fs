7ec499e86d03a9dfe8e7f8ba507e7cf12322b7f1
Sarah Marshall <sarah.marshall@microsoft.com>
2020-12-02 20:01:49 -0800

    Rename data to context in Rewriter

--- Formatter/SyntaxTree/Rewriter.fs
+++ Formatter/SyntaxTree/Rewriter.fs
@@ -6,212 +6,220 @@ open QsFmt.Formatter.SyntaxTree.Node
 open QsFmt.Formatter.SyntaxTree.Statement
 open QsFmt.Formatter.SyntaxTree.Type
 
-type internal 'data Rewriter() =
-    abstract Program: 'data -> Program -> Program
+type internal 'context Rewriter() =
+    abstract Program: 'context -> Program -> Program
 
-    abstract Namespace: 'data -> Namespace -> Namespace
+    abstract Namespace: 'context -> Namespace -> Namespace
 
-    abstract NamespaceElement: 'data -> NamespaceElement -> NamespaceElement
+    abstract NamespaceElement: 'context -> NamespaceElement -> NamespaceElement
 
-    abstract CallableDeclaration: 'data -> CallableDeclaration -> CallableDeclaration
+    abstract CallableDeclaration: 'context -> CallableDeclaration -> CallableDeclaration
 
-    abstract Type: 'data -> Type -> Type
+    abstract Type: 'context -> Type -> Type
 
-    abstract TupleType: 'data -> TupleType -> TupleType
+    abstract TupleType: 'context -> TupleType -> TupleType
 
-    abstract ArrayType: 'data -> ArrayType -> ArrayType
+    abstract ArrayType: 'context -> ArrayType -> ArrayType
 
-    abstract CallableType: 'data -> CallableType -> CallableType
+    abstract CallableType: 'context -> CallableType -> CallableType
 
-    abstract CharacteristicSection: 'data -> CharacteristicSection -> CharacteristicSection
+    abstract CharacteristicSection: 'context -> CharacteristicSection -> CharacteristicSection
 
-    abstract CharacteristicGroup: 'data -> CharacteristicGroup -> CharacteristicGroup
+    abstract CharacteristicGroup: 'context -> CharacteristicGroup -> CharacteristicGroup
 
-    abstract CharacteristicBinaryOperator: 'data -> CharacteristicBinaryOperator -> CharacteristicBinaryOperator
+    abstract CharacteristicBinaryOperator: 'context -> CharacteristicBinaryOperator -> CharacteristicBinaryOperator
 
-    abstract Characteristic: 'data -> Characteristic -> Characteristic
+    abstract Characteristic: 'context -> Characteristic -> Characteristic
 
-    abstract Statement: 'data -> Statement -> Statement
+    abstract Statement: 'context -> Statement -> Statement
 
-    abstract Let: 'data -> Let -> Let
+    abstract Let: 'context -> Let -> Let
 
-    abstract Return: 'data -> Return -> Return
+    abstract Return: 'context -> Return -> Return
 
-    abstract SymbolBinding: 'data -> SymbolBinding -> SymbolBinding
+    abstract SymbolBinding: 'context -> SymbolBinding -> SymbolBinding
 
-    abstract Expression: 'data -> Expression -> Expression
+    abstract Expression: 'context -> Expression -> Expression
 
-    abstract Tuple: 'data -> Tuple -> Tuple
+    abstract Tuple: 'context -> Tuple -> Tuple
 
-    abstract BinaryOperator: 'data -> BinaryOperator -> BinaryOperator
+    abstract BinaryOperator: 'context -> BinaryOperator -> BinaryOperator
 
-    abstract Update: 'data -> Update -> Update
+    abstract Update: 'context -> Update -> Update
 
-    abstract SequenceItem: 'data -> ('data -> 'a -> 'a) -> 'a SequenceItem -> 'a SequenceItem
+    abstract SequenceItem: 'context -> ('context -> 'a -> 'a) -> 'a SequenceItem -> 'a SequenceItem
 
-    abstract Terminal: 'data -> Terminal -> Terminal
+    abstract Terminal: 'context -> Terminal -> Terminal
 
-    default rewriter.Program data program =
+    default rewriter.Program context program =
         { Namespaces =
               program.Namespaces
-              |> List.map (rewriter.Namespace data)
-          Eof = rewriter.Terminal data program.Eof }
+              |> List.map (rewriter.Namespace context)
+          Eof = rewriter.Terminal context program.Eof }
 
-    default rewriter.Namespace data ns =
-        { NamespaceKeyword = rewriter.Terminal data ns.NamespaceKeyword
-          Name = rewriter.Terminal data ns.Name
-          OpenBrace = rewriter.Terminal data ns.OpenBrace
+    default rewriter.Namespace context ns =
+        { NamespaceKeyword = rewriter.Terminal context ns.NamespaceKeyword
+          Name = rewriter.Terminal context ns.Name
+          OpenBrace = rewriter.Terminal context ns.OpenBrace
           Elements =
               ns.Elements
-              |> List.map (rewriter.NamespaceElement data)
-          CloseBrace = rewriter.Terminal data ns.CloseBrace }
+              |> List.map (rewriter.NamespaceElement context)
+          CloseBrace = rewriter.Terminal context ns.CloseBrace }
 
-    default rewriter.NamespaceElement data (CallableDeclaration callable) =
+    default rewriter.NamespaceElement context (CallableDeclaration callable) =
         callable
-        |> rewriter.CallableDeclaration data
+        |> rewriter.CallableDeclaration context
         |> CallableDeclaration
 
-    default rewriter.CallableDeclaration data callable =
-        { CallableKeyword = rewriter.Terminal data callable.CallableKeyword
-          Name = rewriter.Terminal data callable.Name
-          Colon = rewriter.Terminal data callable.Colon
-          ReturnType = rewriter.Type data callable.ReturnType
-          OpenBrace = rewriter.Terminal data callable.OpenBrace
+    default rewriter.CallableDeclaration context callable =
+        { CallableKeyword = rewriter.Terminal context callable.CallableKeyword
+          Name = rewriter.Terminal context callable.Name
+          Colon = rewriter.Terminal context callable.Colon
+          ReturnType = rewriter.Type context callable.ReturnType
+          OpenBrace = rewriter.Terminal context callable.OpenBrace
           Statements =
               callable.Statements
-              |> List.map (rewriter.Statement data)
-          CloseBrace = rewriter.Terminal data callable.CloseBrace }
+              |> List.map (rewriter.Statement context)
+          CloseBrace = rewriter.Terminal context callable.CloseBrace }
 
-    default rewriter.Type data ty =
+    default rewriter.Type context ty =
         match ty with
-        | MissingType terminal -> terminal |> rewriter.Terminal data |> MissingType
+        | MissingType terminal ->
+            terminal
+            |> rewriter.Terminal context
+            |> MissingType
         | TypeParameter terminal ->
             terminal
-            |> rewriter.Terminal data
+            |> rewriter.Terminal context
             |> TypeParameter
-        | BuiltInType terminal -> terminal |> rewriter.Terminal data |> BuiltInType
+        | BuiltInType terminal ->
+            terminal
+            |> rewriter.Terminal context
+            |> BuiltInType
         | UserDefinedType terminal ->
             terminal
-            |> rewriter.Terminal data
+            |> rewriter.Terminal context
             |> UserDefinedType
-        | TupleType tuple -> tuple |> rewriter.TupleType data |> TupleType
-        | ArrayType array -> array |> rewriter.ArrayType data |> ArrayType
+        | TupleType tuple -> tuple |> rewriter.TupleType context |> TupleType
+        | ArrayType array -> array |> rewriter.ArrayType context |> ArrayType
         | CallableType callable ->
             callable
-            |> rewriter.CallableType data
+            |> rewriter.CallableType context
             |> CallableType
 
-    default rewriter.TupleType data tuple =
-        { OpenParen = rewriter.Terminal data tuple.OpenParen
+    default rewriter.TupleType context tuple =
+        { OpenParen = rewriter.Terminal context tuple.OpenParen
           Items =
               tuple.Items
-              |> List.map (rewriter.SequenceItem data rewriter.Type)
-          CloseParen = rewriter.Terminal data tuple.CloseParen }
+              |> List.map (rewriter.SequenceItem context rewriter.Type)
+          CloseParen = rewriter.Terminal context tuple.CloseParen }
 
-    default rewriter.ArrayType data array =
-        { BaseType = rewriter.Type data array.BaseType
-          OpenBracket = rewriter.Terminal data array.OpenBracket
-          CloseBracket = rewriter.Terminal data array.CloseBracket }
+    default rewriter.ArrayType context array =
+        { BaseType = rewriter.Type context array.BaseType
+          OpenBracket = rewriter.Terminal context array.OpenBracket
+          CloseBracket = rewriter.Terminal context array.CloseBracket }
 
-    default rewriter.CallableType data callable =
-        { OpenParen = rewriter.Terminal data callable.OpenParen
-          InnerOpenParen = rewriter.Terminal data callable.InnerOpenParen
-          FromType = rewriter.Type data callable.FromType
-          Arrow = rewriter.Terminal data callable.Arrow
-          ToType = rewriter.Type data callable.ToType
-          InnerCloseParen = rewriter.Terminal data callable.InnerCloseParen
+    default rewriter.CallableType context callable =
+        { OpenParen = rewriter.Terminal context callable.OpenParen
+          InnerOpenParen = rewriter.Terminal context callable.InnerOpenParen
+          FromType = rewriter.Type context callable.FromType
+          Arrow = rewriter.Terminal context callable.Arrow
+          ToType = rewriter.Type context callable.ToType
+          InnerCloseParen = rewriter.Terminal context callable.InnerCloseParen
           Characteristics =
               callable.Characteristics
-              |> Option.map (rewriter.CharacteristicSection data)
-          CloseParen = rewriter.Terminal data callable.CloseParen }
+              |> Option.map (rewriter.CharacteristicSection context)
+          CloseParen = rewriter.Terminal context callable.CloseParen }
 
-    default rewriter.CharacteristicSection data section =
-        { IsKeyword = rewriter.Terminal data section.IsKeyword
-          Characteristic = rewriter.Characteristic data section.Characteristic }
+    default rewriter.CharacteristicSection context section =
+        { IsKeyword = rewriter.Terminal context section.IsKeyword
+          Characteristic = rewriter.Characteristic context section.Characteristic }
 
-    default rewriter.CharacteristicGroup data group =
-        { OpenParen = rewriter.Terminal data group.OpenParen
-          Characteristic = rewriter.Characteristic data group.Characteristic
-          CloseParen = rewriter.Terminal data group.CloseParen }
+    default rewriter.CharacteristicGroup context group =
+        { OpenParen = rewriter.Terminal context group.OpenParen
+          Characteristic = rewriter.Characteristic context group.Characteristic
+          CloseParen = rewriter.Terminal context group.CloseParen }
 
-    default rewriter.CharacteristicBinaryOperator data operator =
-        { Left = rewriter.Characteristic data operator.Left
-          Operator = rewriter.Terminal data operator.Operator
-          Right = rewriter.Characteristic data operator.Right }
+    default rewriter.CharacteristicBinaryOperator context operator =
+        { Left = rewriter.Characteristic context operator.Left
+          Operator = rewriter.Terminal context operator.Operator
+          Right = rewriter.Characteristic context operator.Right }
 
-    default rewriter.Characteristic data characteristic =
+    default rewriter.Characteristic context characteristic =
         match characteristic with
         | Adjoint -> Adjoint
         | Controlled -> Controlled
         | CharacteristicGroup group ->
             group
-            |> rewriter.CharacteristicGroup data
+            |> rewriter.CharacteristicGroup context
             |> CharacteristicGroup
         | CharacteristicBinaryOperator operator ->
             operator
-            |> rewriter.CharacteristicBinaryOperator data
+            |> rewriter.CharacteristicBinaryOperator context
             |> CharacteristicBinaryOperator
 
-    default rewriter.Statement data statement =
+    default rewriter.Statement context statement =
         match statement with
-        | Let lets -> lets |> rewriter.Let data |> Let
-        | Return returns -> returns |> rewriter.Return data |> Return
+        | Let lets -> lets |> rewriter.Let context |> Let
+        | Return returns -> returns |> rewriter.Return context |> Return
 
-    default rewriter.Let data lets =
-        { LetKeyword = rewriter.Terminal data lets.LetKeyword
-          Binding = rewriter.SymbolBinding data lets.Binding
-          Equals = rewriter.Terminal data lets.Equals
-          Value = rewriter.Expression data lets.Value
-          Semicolon = rewriter.Terminal data lets.Semicolon }
+    default rewriter.Let context lets =
+        { LetKeyword = rewriter.Terminal context lets.LetKeyword
+          Binding = rewriter.SymbolBinding context lets.Binding
+          Equals = rewriter.Terminal context lets.Equals
+          Value = rewriter.Expression context lets.Value
+          Semicolon = rewriter.Terminal context lets.Semicolon }
 
-    default rewriter.Return data returns =
-        { ReturnKeyword = rewriter.Terminal data returns.ReturnKeyword
-          Expression = rewriter.Expression data returns.Expression
-          Semicolon = rewriter.Terminal data returns.Semicolon }
+    default rewriter.Return context returns =
+        { ReturnKeyword = rewriter.Terminal context returns.ReturnKeyword
+          Expression = rewriter.Expression context returns.Expression
+          Semicolon = rewriter.Terminal context returns.Semicolon }
 
-    default rewriter.SymbolBinding data binding =
+    default rewriter.SymbolBinding context binding =
         match binding with
-        | SymbolName name -> rewriter.Terminal data name |> SymbolName
+        | SymbolName name -> rewriter.Terminal context name |> SymbolName
         | SymbolTuple bindings ->
             bindings
-            |> List.map (rewriter.SymbolBinding data)
+            |> List.map (rewriter.SymbolBinding context)
             |> SymbolTuple
 
-    default rewriter.Expression data expression =
+    default rewriter.Expression context expression =
         match expression with
         | MissingExpression terminal ->
             terminal
-            |> rewriter.Terminal data
+            |> rewriter.Terminal context
             |> MissingExpression
-        | Literal literal -> literal |> rewriter.Terminal data |> Literal
-        | Tuple tuple -> tuple |> rewriter.Tuple data |> Tuple
+        | Literal literal -> literal |> rewriter.Terminal context |> Literal
+        | Tuple tuple -> tuple |> rewriter.Tuple context |> Tuple
         | BinaryOperator operator ->
             operator
-            |> rewriter.BinaryOperator data
+            |> rewriter.BinaryOperator context
             |> BinaryOperator
-        | Update update -> update |> rewriter.Update data |> Update
+        | Update update -> update |> rewriter.Update context |> Update
 
-    default rewriter.Tuple data tuple =
-        { OpenParen = rewriter.Terminal data tuple.OpenParen
+    default rewriter.Tuple context tuple =
+        { OpenParen = rewriter.Terminal context tuple.OpenParen
           Items =
               tuple.Items
-              |> List.map (rewriter.SequenceItem data rewriter.Expression)
-          CloseParen = rewriter.Terminal data tuple.CloseParen }
+              |> List.map (rewriter.SequenceItem context rewriter.Expression)
+          CloseParen = rewriter.Terminal context tuple.CloseParen }
 
-    default rewriter.BinaryOperator data operator =
-        { Left = rewriter.Expression data operator.Left
-          Operator = rewriter.Terminal data operator.Operator
-          Right = rewriter.Expression data operator.Right }
+    default rewriter.BinaryOperator context operator =
+        { Left = rewriter.Expression context operator.Left
+          Operator = rewriter.Terminal context operator.Operator
+          Right = rewriter.Expression context operator.Right }
 
-    default rewriter.Update data update =
-        { Record = rewriter.Expression data update.Record
-          With = rewriter.Terminal data update.With
-          Item = rewriter.Expression data update.Item
-          Arrow = rewriter.Terminal data update.Arrow
-          Value = rewriter.Expression data update.Value }
+    default rewriter.Update context update =
+        { Record = rewriter.Expression context update.Record
+          With = rewriter.Terminal context update.With
+          Item = rewriter.Expression context update.Item
+          Arrow = rewriter.Terminal context update.Arrow
+          Value = rewriter.Expression context update.Value }
 
-    default rewriter.SequenceItem data f item =
-        { Item = item.Item |> Option.map (f data)
-          Comma = item.Comma |> Option.map (rewriter.Terminal data) }
+    default rewriter.SequenceItem context f item =
+        { Item = item.Item |> Option.map (f context)
+          Comma =
+              item.Comma
+              |> Option.map (rewriter.Terminal context) }
 
     default _.Terminal _ terminal = terminal
