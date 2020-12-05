897ffc856c617064222a87c5dedb96e1447e5c20
Sarah Marshall <sarah.marshall@microsoft.com>
2020-12-04 19:09:17 -0800

    Discover test cases with attributes

---
+++ Formatter.Tests/Discoverer.fs
@@ -0,0 +1,50 @@
+﻿namespace QsFmt.Formatter.Tests
+
+open QsFmt.Formatter
+open System
+open System.Linq
+open System.Reflection
+open Xunit
+
+type internal ExampleAttribute() =
+    inherit Attribute()
+
+type internal FixedPointAttribute() =
+    inherit Attribute()
+
+module Discoverer =
+    let private cases (attribute: Type) =
+        Assembly.GetCallingAssembly().GetTypes()
+        |> Seq.collect (fun typ -> typ.GetProperties())
+        |> Seq.filter (fun property ->
+            property.GetCustomAttributes attribute
+            |> Seq.isEmpty
+            |> not)
+        |> Seq.map (fun property -> property.GetValue null)
+        |> fun values -> values.OfType<'a>()
+
+    type private ExampleData() as data =
+        inherit TheoryData<string, string>()
+
+        do
+            cases typeof<ExampleAttribute>
+            |> Seq.iter data.Add
+
+    type private FixedPointData() as data =
+        inherit TheoryData<string>()
+
+        do
+            cases typeof<ExampleAttribute>
+            |> Seq.map snd<string, _>
+            |> Seq.append (cases typeof<FixedPointAttribute>)
+            |> Seq.iter data.Add
+
+    [<Theory>]
+    [<ClassData(typeof<ExampleData>)>]
+    let ``Code is formatted correctly`` input output =
+        Assert.Equal(output, Formatter.format input)
+
+    [<Theory>]
+    [<ClassData(typeof<FixedPointData>)>]
+    let ``Formatted code is unchanged`` input =
+        Assert.Equal(input, Formatter.format input)
---
+++ Formatter.Tests/FixedPoints.fs
@@ -0,0 +1,26 @@
+﻿module QsFmt.Formatter.Tests.AlreadyFormatted
+
+open QsFmt.Formatter.Tests
+
+[<FixedPoint>]
+let ``Preserves namespace comments`` = """/// The Foo namespace.
+namespace Foo {}
+
+/// The Bar namespace.
+namespace Bar {}
+
+// End of file."""
+
+[<FixedPoint>]
+let ``Preserves function with one parameter`` = """namespace Foo {
+    function Bar(x : Int) : Int {
+        return x;
+    }
+}"""
+
+[<FixedPoint>]
+let ``Preserves function with two parameters`` = """namespace Foo {
+    function Bar(x : Int, y : Int) : Int {
+        return x + y;
+    }
+}"""
