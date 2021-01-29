c7511917095fa50d30debbbf5151e262229ac0f4
Sarah Marshall <sarah.marshall@microsoft.com>
2021-01-29 14:10:39 -0800

    Add docs to SyntaxTree/Statement.fs

--- Formatter/SyntaxTree/Statement.fs
+++ Formatter/SyntaxTree/Statement.fs
@@ -1,42 +1,89 @@
 ﻿namespace QsFmt.Formatter.SyntaxTree
 
+/// A declaration for a new symbol.
 type internal SymbolDeclaration =
-    { Name: Terminal
+    { /// The name of the symbol.
+      Name: Terminal
+
+      /// The type of the symbol.
       Type: TypeAnnotation option }
 
+/// A binding for one or more new symbols.
 type internal SymbolBinding =
+    /// A declaration for a new symbol.
     | SymbolDeclaration of SymbolDeclaration
+
+    /// A declaration for a tuple of new symbols.
     | SymbolTuple of SymbolBinding Tuple
 
+/// A "let" statement.
 type internal Let =
-    { LetKeyword: Terminal
+    { /// The "let" keyword.
+      LetKeyword: Terminal
+
+      /// The symbol binding.
       Binding: SymbolBinding
+
+      /// The equals symbol.
       Equals: Terminal
+
+      /// The value of the symbol binding.
       Value: Expression
+
+      /// The semicolon.
       Semicolon: Terminal }
 
+/// A "return" statement.
 type internal Return =
-    { ReturnKeyword: Terminal
+    { /// The "return" keyword.
+      ReturnKeyword: Terminal
+
+      /// The returned expression.
       Expression: Expression
+
+      /// The semicolon.
       Semicolon: Terminal }
 
+/// An "if" statement.
 type internal If =
-    { IfKeyword: Terminal
+    { /// The "if" keyword.
+      IfKeyword: Terminal
+
+      /// The condition under which to execute the block.
       Condition: Expression
+
+      /// The conditional block.
       Block: Statement Block }
 
+/// An "else" statement.
 and internal Else =
-    { ElseKeyword: Terminal
+    { /// The "else" keyword.
+      ElseKeyword: Terminal
+
+      /// The conditional block.
       Block: Statement Block }
 
+/// A statement.
 and internal Statement =
+    /// A "let" statement.
     | Let of Let
+
+    /// A "return" statement.
     | Return of Return
+
+    /// An "if" statement.
     | If of If
+
+    /// An "else" statement.
     | Else of Else
+
+    /// An unknown statement.
     | Unknown of Terminal
 
 module internal Statement =
+    /// <summary>
+    /// Maps a statement by applying <paramref name="mapper"/> to its trivia prefix.
+    /// </summary>
     let mapPrefix mapper =
         function
         | Let lets ->
